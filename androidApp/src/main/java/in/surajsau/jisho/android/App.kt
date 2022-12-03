9814b0949b0cab9c21c7dd23c4da6fac8736fd74
Suraj Kumar Sau <i@surajsau.in>
2022-12-04 01:44:19 +0900

    :beetle: Fixed compilation error

--- androidApp/src/main/java/in/surajsau/jisho/android/App.kt
+++ androidApp/src/main/java/in/surajsau/jisho/android/App.kt
@@ -2,8 +2,8 @@ package `in`.surajsau.jisho.android
 
 import android.app.Application
 import `in`.surajsau.jisho.app.AppModule
-import `in`.surajsau.jisho.di.initKoin
 import `in`.surajsau.jisho.download.DownloadModule
+import `in`.surajsau.jisho.initKoin
 import org.koin.android.ext.koin.androidContext
 
 class App : Application() {
