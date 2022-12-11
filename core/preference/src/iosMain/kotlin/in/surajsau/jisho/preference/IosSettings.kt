db4ddfdb12b0e102fc1f5d937d48120ad2967b42
Suraj Kumar Sau <i@surajsau.in>
2022-12-11 19:28:46 +0900

    :arrow_up: Updated native coroutines version

--- core/preference/src/iosMain/kotlin/in/surajsau/jisho/preference/IosSettings.kt
+++ core/preference/src/iosMain/kotlin/in/surajsau/jisho/preference/IosSettings.kt
@@ -1,11 +1,13 @@
 package `in`.surajsau.jisho.preference
 
+import com.russhwolf.settings.ExperimentalSettingsApi
 import com.russhwolf.settings.NSUserDefaultsSettings
 import com.russhwolf.settings.coroutines.FlowSettings
 import com.russhwolf.settings.coroutines.toFlowSettings
 import org.koin.core.scope.Scope
 import platform.Foundation.NSUserDefaults
 
+@OptIn(ExperimentalSettingsApi::class)
 public actual fun Scope.settings(): FlowSettings {
     return NSUserDefaultsSettings(NSUserDefaults.standardUserDefaults).toFlowSettings()
 }
--- core/viewmodel/build.gradle.kts
+++ core/viewmodel/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("jisho.convention.kmp")
-    id("com.rickclephas.kmp.nativecoroutines") version "0.11.4-new-mm"
+    id("com.rickclephas.kmp.nativecoroutines") version "0.13.0"
     id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
 }
 
